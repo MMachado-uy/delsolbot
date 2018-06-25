35e45910580d8b11d83fb553598932257ffde410
Mauricio Machado <macmauricio@gmail.com>
2018-06-25 20:43:15 -0300

    Some cleanup

--- database/seed.sql
+++ database/seed.sql
@@ -1,9 +1,9 @@
 INSERT INTO `sources` VALUES 
-    (1,'https://delsol.uy/feed/notoquennada','@NoToquenNadaTelegram', 'No Toquen Nada'),
-    (2,'https://delsol.uy/feed/darwin','@DarwinDesbocattiTelegram', 'Darwin Desbocatti'),
-    (3,'https://delsol.uy/feed/quientedice','@QuienTeDiceTelegram', 'Quién te Dice?'),
-    (4,'https://delsol.uy/feed/lamesa','@LaMesaTelegram', 'La Mesa de los Galanes'),
-    (5,'https://delsol.uy/feed/facildesviarse','@FacilDesviarseTelegram', 'Fácil Desviarse'),
-    (6,'https://delsol.uy/feed/13a0','@TreceACeroTelegram', 'Trece a Cero'),
-    (7,'https://delsol.uy/feed/dolina','@LaVenganzaTelegram', 'La Vengaza Será Terrible'),
-    (8,'https://delsol.uy/feed/abran','@AbranCanchaTelegram', 'Abran Cancha');
+    (1,'https://delsol.uy/feed/notoquennada','@NoToquenNadaTelegram'),
+    (2,'https://delsol.uy/feed/darwin','@DarwinDesbocattiTelegram'),
+    (3,'https://delsol.uy/feed/quientedice','@QuienTeDiceTelegram'),
+    (4,'https://delsol.uy/feed/lamesa','@LaMesaTelegram'),
+    (5,'https://delsol.uy/feed/facildesviarse','@FacilDesviarseTelegram'),
+    (6,'https://delsol.uy/feed/13a0','@TreceACeroTelegram'),
+    (7,'https://delsol.uy/feed/dolina','@LaVenganzaTelegram'),
+    (8,'https://delsol.uy/feed/abran','@AbranCanchaTelegram');
--- database/update_001.sql
+++ database/update_001.sql
@@ -5,3 +5,12 @@ ALTER TABLE sources
 ALTER TABLE podcasts
     ADD COLUMN file_id VARCHAR(50);
     MODIFY COLUMN obs TEXT;
+
+UPDATE `sources` SET nombre='No Toquen Nada' WHERE channel='@NoToquenNadaTelegram'
+UPDATE `sources` SET nombre='Darwin Desbocatti' WHERE channel='@DarwinDesbocattiTelegram'
+UPDATE `sources` SET nombre='Quién te Dice?' WHERE channel='@QuienTeDiceTelegram'
+UPDATE `sources` SET nombre='La Mesa de los Galanes' WHERE channel='@LaMesaTelegram'
+UPDATE `sources` SET nombre='Fácil Desviarse' WHERE channel='@FacilDesviarseTelegram'
+UPDATE `sources` SET nombre='Trece a Cero' WHERE channel='@TreceACeroTelegram'
+UPDATE `sources` SET nombre='La Venganza Será Terrible' WHERE channel='@LaVenganzaTelegram'
+UPDATE `sources` SET nombre='Abran Cancha' WHERE channel='@AbranCanchaTelegram'
