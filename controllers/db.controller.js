ee63c1aca5a6a1e639f82a9f50bbcbe3397936b4
Mauricio Machado <macmauricio@gmail.com>
2020-11-29 16:16:20 +0000

    Fixed envvar

--- controllers/db.controller.js
+++ controllers/db.controller.js
@@ -8,7 +8,7 @@ const mysql = require('mysql').createPool({
     host            : process.env.DB_HOST,
     user            : process.env.DB_USER,
     password        : process.env.DB_PASS,
-    database        : process.env.DB_DB
+    database        : process.env.DB
 });
 const {
     parseResponse
