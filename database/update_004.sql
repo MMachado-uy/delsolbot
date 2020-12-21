20dee6a6f2c7398da7ebfc2c4b0b026609977426
Mauricio Machado <mauricio@upshow.tv>
2020-12-21 10:46:59 -0300

    Cambio en migration

--- database/update_004.sql
+++ database/update_004.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `podcasts`
     ADD COLUMN `title` VARCHAR(120) AFTER `archivo`,
     ADD COLUMN `caption` TEXT AFTER `title`,
-    ADD COLUMN `url` URL AFTER `caption`,
+    ADD COLUMN `url` VARCHAR(120) AFTER `caption`,
     ADD COLUMN `msg_id` VARCHAR(20) AFTER `pudo_subir`;
