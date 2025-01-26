79c86fe94b41afc915d3fa265377febcbcc49eb4
Mauricio Machado <mauricio@upshow.tv>
2025-01-25 21:46:22 -0300

    Add splitter

--- app.js
+++ app.js
@@ -1,113 +1,106 @@
 require('dotenv').config();
 
+const path = require('path');
+const fs = require('fs');
+
 const CronJob = require('cron').CronJob;
 const FormData = require('form-data');
 const NodeID3 = require('node-id3');
 const axios = require('axios');
-const fs = require('fs');
 
-const TwController = require('./controllers/twitter.controller');
 const DbController = require('./controllers/db.controller');
 const DB = new DbController();
 const {
-  cleanDownloads,
-  debug,
-  log,
-  logError,
-  getIdFromItem,
-  getMedia,
-  sanitizeContent,
-  sanitizeEpisode,
-  getFeed
+    cleanDownloads,
+    debug,
+    log,
+    logError,
+    getIdFromItem,
+    getMedia,
+    sanitizeContent,
+    sanitizeEpisode,
+    getFeed,
+    getFileSizeInMB,
+    pathToTitle
 } = require('./lib/helpers');
-
+const { splitEpisode } = require('./lib/splitter');
 
 const {
-  BOT_TOKEN,
-  TEST_CHANNEL,
-  CRON_MAIN,
-  NODE_ENV: ENV
+    BOT_TOKEN,
+    TEST_CHANNEL,
+    CRON_MAIN,
+    NODE_ENV: ENV
 } = process.env;
 const COVER = './assets/cover.jpg';
 const DDIR = './downloads/';
-
+const TELEGRAM_THRESHOLD = 50;
 
 const mainCron = new CronJob(CRON_MAIN, () => {
-  main().catch(e => {
-    logError(e);
-  });
+    main().catch(e => {
+        logError(e);
+    });
 }, null);
 
 /**
  * Main Application logic
  */
 const main = async () => {
-  try {
-    const rssList = await DB.getRssList();
-    debug(`Found ${rssList.length} rss sources`);
+    try {
+        const rssList = await DB.getRssList();
+        debug(`Found ${rssList.length} rss sources`);
 
-    for (const rssSource of rssList) {
-      log(`Starting to process ${rssSource.channel}`);
+        for (const rssSource of rssList) {
+        log(`Starting to process ${rssSource.channel}`);
 
-      await processFeed(rssSource);
+        await processFeed(rssSource);
 
-      log(`Finished processing ${rssSource.channel}`);
+        log(`Finished processing ${rssSource.channel}`);
+        }
+    } catch (error) {
+        logError(`Error in main process: ${error}`);
+    } finally {
+        cleanDownloads(DDIR);
     }
-  } catch (error) {
-    logError(`Error in main process: ${error}`);
-  } finally {
-    cleanDownloads(DDIR);
-  }
 }
 
 const processFeed = async rssSource => {
-  const feed = await getFeed(rssSource.url);
-  feed.items.map(item => {
-    item.channel = rssSource.channel;
+    const feed = await getFeed(rssSource.url);
+    feed.items.map(item => {
+        item.channel = rssSource.channel;
 
-    return item;
-  });
+        return item;
+    });
 
-  const { title } = feed;
+    const { title } = feed;
 
-  feed.items.forEach(async item => {
-    await processItem(item, title);
-  })
+    for await (const item of feed.items) {
+        await processItem(item, title);
+    }
 }
 
 const processItem = async (item, title) => {
-  const itemId = getIdFromItem(item);
-  debug(`Processing item: ${itemId}`);
+    const itemId = getIdFromItem(item);
+    debug(`Processing item: ${itemId}`);
 
-  try {
-    const stored = await DB.getPodcastById(itemId);
+    try {
+        const stored = await DB.getPodcastById(itemId);
 
-    const isForward = stored &&
-                      stored.length &&
-                      stored[0].pudo_subir &&
-                      !stored.some(record => record.channel === item.channel);
+        const isForward = stored &&
+                        stored.length &&
+                        stored[0].pudo_subir &&
+                        !stored.some(record => record.channel === item.channel);
 
-    if (isForward) item.forwardFile = stored[0].file_id;
+        if (isForward) item.forwardFile = stored[0].file_id;
 
-    if (isForward || stored.length === 0) {
-      const telegramMessage = await sendToTelegram(item, title);
-      const { message_id: messageId, imagePath } = telegramMessage;
+        if (isForward || stored.length === 0) {
+            await sendToTelegram(item, title);
+            debug('Sent!');
+        }
 
-      debug('Sending to Twitter...');
-
-      try {
-        if (ENV === 'prod') await sendToTwitter(messageId, imagePath, item.title, item.channel);
-      } catch (error) {
-        log('Elon finally did it - Unable to post to Twitter API :: ', error);
-      }
-
-      debug('Sent!');
+        log(`Done processing item: ${itemId}`);
+    } catch (error) {
+        logError(`Error processing item ${itemId}:`, error);
     }
-
-    log(`Done processing item: ${itemId}`);
-  } catch (error) {
-    logError(`Error processing item ${itemId}:`, error);
-  }
 };
 
 /**
@@ -116,41 +109,60 @@ const processItem = async (item, title) => {
  * @returns telegramMessage - see: https://core.telegram.org/bots/api#message
  */
 const sendToTelegram = async (feedItem, channelName) => {
-  const { title, content, link: url, channel, itunes: { image } } = feedItem;
-  const archivo = getIdFromItem(feedItem);
-  const caption = `<b>${title}</b>\n${content}`;
-  const folderName = sanitizeContent(channelName);
-  const episodePath = `${DDIR}${folderName}/${sanitizeEpisode(title)}.mp3`;
+    const { title, content, link: url, channel, itunes: { image } } = feedItem;
+    const fileId = getIdFromItem(feedItem);
+    const folderName = sanitizeContent(channelName);
+    const fileName = `${sanitizeEpisode(title)}.mp3`;
+    let caption = `<b>${title}</b>\n${content}`;
+    let episodePaths = [path.join(DDIR, folderName, fileName)];
+    let hadToSplit = false;
 
-  try {
-    const forward = feedItem.forwardFile ? feedItem.forwardFile : null;
-    const imagePath = await downloadImage(image, folderName);
+    try {
+        const forward = feedItem.forwardFile ? feedItem.forwardFile : null;
+        const imagePath = await downloadImage(image, folderName);
 
-    if (!forward) {
-      await downloadEpisode(url, episodePath, folderName);
-      editMetadata(channelName, title, caption, episodePath, archivo, imagePath);
+        if (!forward) {
+            await downloadEpisode(url, episodePaths[0], folderName);
+        }
+
+        if (getFileSizeInMB(episodePaths[0]) > TELEGRAM_THRESHOLD) {
+            hadToSplit = true;
+            episodePaths = await splitEpisode(fileName, path.join(DDIR, folderName));
+        }
+
+        for (let i = 0; i < episodePaths.length; i++) {
+            const part = episodePaths[i];
+            await editMetadata(channelName, pathToTitle(part), caption, part, `${fileId}-${i}`, imagePath);
+        }
+
+        let success = true;
+        for (const [i, episodePath] of episodePaths.entries()) {
+            try {
+                if (hadToSplit) caption = `(Parte ${i}) ${caption}`;
+
+                debug({ episodePath, caption, channel, channelName, title: pathToTitle(episodePath), fileId, forward });
+                const telegramResponse = await sendEpisodeToChannel(episodePath, `${caption}`, channel, channelName, pathToTitle(episodePath), fileId, forward);
+
+                debug(telegramResponse);
+                debug(`${fileId} Uploaded`);
+
+                const { file_id } = telegramResponse.result.audio;
+                const { message_id } = telegramResponse.result;
+                await DB.registerUpload(fileId, '', true, file_id, channel, pathToTitle(episodePath), caption, url, message_id);
+
+                success = success && true;
+            } catch (error) {
+                success = false;
+            }
+        }
+
+        return success;
+    } catch (err) {
+        logError(`${fileId} Failed to upload. ${err.message ?? err}`);
+        await DB.registerUpload(fileId, err.response?.body?.description ?? err.message, false, '', channel, title, caption, url);
+
+        throw err;
     }
-
-    const telegramResponse = await sendEpisodeToChannel(episodePath, caption, channel, channelName, title, archivo, forward);
-
-    debug(telegramResponse);
-    debug(`${archivo} Uploaded`);
-
-    const { file_id: fileId } = telegramResponse.result.audio;
-    const { message_id } = telegramResponse.result;
-    await DB.registerUpload(archivo, '', true, fileId, channel, title, caption, url, message_id);
-
-    return { ...telegramResponse.result, imagePath };
-  } catch (err) {
-    logError(`${archivo} Failed to upload. ${err.message ?? err}`);
-    await DB.registerUpload(archivo, err.response?.body?.description ?? err.message, false, '', channel, title, caption, url);
-
-    throw err;
-  }
-};
-
-const sendToTwitter = (messageId, imagePath, title, channel) => {
-  return new TwController().tweetit(messageId, imagePath, title, channel);
 };
 
 /**
@@ -161,9 +173,9 @@ const sendToTwitter = (messageId, imagePath, title, channel) => {
  * @returns {Promise} La ruta local del episodio
  */
 const downloadEpisode = (episodeUrl, episodePath, folder) => {
-  if (!fs.existsSync(`${DDIR}${folder}`)) fs.mkdirSync(`${DDIR}${folder}`);
+    if (!fs.existsSync(`${DDIR}${folder}`)) fs.mkdirSync(`${DDIR}${folder}`);
 
-  return getMedia(episodeUrl, episodePath);
+    return getMedia(episodeUrl, episodePath);
 };
 
 /**
@@ -173,16 +185,16 @@ const downloadEpisode = (episodeUrl, episodePath, folder) => {
  * @returns {Promise} La ruta local de la imagen
  */
 const downloadImage = async (imageUrl, folder) => {
-  if (typeof imageUrl !== 'string') return COVER;
+    if (typeof imageUrl !== 'string') return COVER;
 
-  const downloadFolder = `${DDIR}${folder}`;
+    const downloadFolder = `${DDIR}${folder}`;
 
-  if (!fs.existsSync(downloadFolder)) fs.mkdirSync(downloadFolder);
+    if (!fs.existsSync(downloadFolder)) fs.mkdirSync(downloadFolder);
 
-  const imgName = imageUrl.split('/').pop();
-  const path = `${downloadFolder}/${imgName}`;
+    const imgName = imageUrl.split('/').pop();
+    const filePath = path.join(downloadFolder, imgName);
 
-  return getMedia(imageUrl, path);
+    return getMedia(imageUrl, filePath);
 };
 
 /**
@@ -192,30 +204,31 @@ const downloadImage = async (imageUrl, folder) => {
  * @param {String} comment - Episode's description, mapped to the 'comment' field
  * @param {String} episodePath -The episode's path
  * @param {String} imagePath - Cover Image for the episode
- * @param {Number} track - Track number, mapped from file number
+ * @param {Number|String} track - Track number, mapped from file number
  */
 const editMetadata = (artist, title, comment, episodePath, track, imagePath = COVER) => {
-  debug('Started Metadating');
+    debug('Started Metadating');
+    debug({artist, title, comment, episodePath, track, imagePath});
 
-  const coverBuffer = fs.readFileSync(imagePath);
-  const episodeBuffer = fs.readFileSync(episodePath);
-  const tags = {
-    artist,
-    title,
-    comment,
-    TRCK: track,
-    image: {
-      mime: 'image/jpeg',
-      type: {
-        id: 3,
-        name: 'front cover'
-      },
-      description: 'front cover',
-      imageBuffer: coverBuffer
-    }
-  };
+    const coverBuffer = fs.readFileSync(imagePath);
+    const episodeBuffer = fs.readFileSync(episodePath);
+    const tags = {
+        artist,
+        title,
+        comment,
+        TRCK: track,
+        image: {
+        mime: 'image/jpeg',
+        type: {
+            id: 3,
+            name: 'front cover'
+        },
+        description: 'front cover',
+        imageBuffer: coverBuffer
+        }
+    };
 
-  return NodeID3.write(tags, episodeBuffer);
+    return NodeID3.write(tags, episodeBuffer);
 };
 
 /**
@@ -230,44 +243,44 @@ const editMetadata = (artist, title, comment, episodePath, track, imagePath = CO
  * @param {String|Number} id The ID of the Episode
  */
 const sendEpisodeToChannel = async (episodePath, caption, chatId, performer, title, id, fileId = null) => {
-  debug(`Sending: ${id}`);
+    debug(`Sending: ${id}`);
 
-  const connectionUrl = `https://api.telegram.org/bot${BOT_TOKEN}/sendAudio`;
-  const destination = ENV === 'prod' ? chatId : TEST_CHANNEL;
+    const connectionUrl = `https://api.telegram.org/bot${BOT_TOKEN}/sendAudio`;
+    const destination = ENV === 'prod' ? chatId : TEST_CHANNEL;
 
-  const file = !fileId ? fs.createReadStream(episodePath) : fileId;
+    const file = !fileId ? fs.createReadStream(episodePath) : fileId;
 
-  const payload = new FormData();
-  payload.append('audio', fileId === null ? file : fileId);
-  payload.append('disable_notification', 'true');
-  payload.append('parse_mode', 'html');
-  payload.append('caption', caption);
-  payload.append('chat_id', destination);
-  payload.append('performer', performer);
-  payload.append('title', title);
+    const payload = new FormData();
+    payload.append('audio', fileId === null ? file : fileId);
+    payload.append('disable_notification', 'true');
+    payload.append('parse_mode', 'html');
+    payload.append('caption', caption);
+    payload.append('chat_id', destination);
+    payload.append('performer', performer);
+    payload.append('title', title);
 
-  try {
-    const { data } = await axios({
-      method: 'post',
-      url: connectionUrl,
-      data: payload,
-      maxContentLength: Infinity,
-      maxBodyLength: Infinity,
-      headers: { 'Content-Type': 'multipart/form-data' }
-    });
+    try {
+        const { data } = await axios({
+            method: 'post',
+            url: connectionUrl,
+            data: payload,
+            maxContentLength: Infinity,
+            maxBodyLength: Infinity,
+            headers: { 'Content-Type': 'multipart/form-data' }
+        });
   
-    return data;  
-  } catch (error) {
-    // If file is a Read Stream, destroy it
-    if (typeof file.destroy === 'function') file.destroy();
-    throw error;
-  }
+        return data;  
+    } catch (error) {
+        // If file is a Read Stream, destroy it
+        if (typeof file.destroy === 'function') file.destroy();
+        throw error;
+    }
 };
 
 if (ENV === 'local') {
-  main().catch(e => {
-    logError(e);
-  });
+    main().catch(e => {
+        logError(e);
+    });
 } else {
-  mainCron.start();
+    mainCron.start();
 }
--- database/seed.sql
+++ database/seed.sql
@@ -1,9 +1,9 @@
 INSERT INTO `sources` VALUES 
-    (1,'https://delsol.uy/feed/notoquennada','@NoToquenNadaTelegram'),
-    (2,'https://delsol.uy/feed/darwin','@DarwinDesbocattiTelegram'),
-    (3,'https://delsol.uy/feed/quientedice','@QuienTeDiceTelegram'),
-    (4,'https://delsol.uy/feed/lamesa','@LaMesaTelegram'),
-    (5,'https://delsol.uy/feed/facildesviarse','@FacilDesviarseTelegram'),
-    (6,'https://delsol.uy/feed/13a0','@TreceACeroTelegram'),
-    (7,'https://delsol.uy/feed/dolina','@LaVenganzaTelegram'),
-    (8,'https://delsol.uy/feed/abran','@AbranCanchaTelegram');
+    (1,'https://delsol.uy/feed/notoquennada','@NoToquenNadaTelegram','No Toquen Nada'),
+    (2,'https://delsol.uy/feed/darwin','@DarwinDesbocattiTelegram','Darwin'),
+    (3,'https://delsol.uy/feed/quientedice','@QuienTeDiceTelegram','Quien Te Dice'),
+    (4,'https://delsol.uy/feed/lamesa','@LaMesaTelegram','La Mesa'),
+    (5,'https://delsol.uy/feed/facildesviarse','@FacilDesviarseTelegram','Facil Desviarse'),
+    (6,'https://delsol.uy/feed/13a0','@TreceACeroTelegram','Trece a Cero'),
+    (7,'https://delsol.uy/feed/dolina','@LaVenganzaTelegram','La Venganza Sera Terrible'),
+    (8,'https://delsol.uy/feed/abran','@AbranCanchaTelegram','Abran Cancha');
